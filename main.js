974fdfc4f065e503d4b73546326af540074b946d
ahona-ms2011 <98218734+ahona-ms2011@users.noreply.github.com>
2022-03-06 12:10:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+   {      
+    last_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+}
+
+canvas.addEventListener("touchmove", my_touchmove);
+
+function my_touchmove(e) 
+{
+    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+    ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
+    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_touch_x = current_position_of_touch_x; 
+    last_position_of_touch_y = current_position_of_touch_y;
+   
+}
+
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
